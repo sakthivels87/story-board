b432ca442fccb95a5ae9b5fba9bb007066ba1ede
Sakthivel <sakthi.samuthiram@gmail.com>
2023-12-06 15:05:11 +0530

    Prisma client update

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,5 +1,7 @@
+"use client";
 import Link from "next/link";
 import { SiStorybook } from "react-icons/si";
+import { usePathname } from "next/navigation";
 import classNames from "classnames";
 
 const NavBar = () => {
@@ -13,6 +15,8 @@ const NavBar = () => {
       href: "/stories",
     },
   ];
+  const currentPath = usePathname();
+  console.log(currentPath, "----current path");
   return (
     <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
       <Link href="/">
@@ -23,7 +27,11 @@ const NavBar = () => {
           <li key={link.href}>
             <Link
               href={link.href}
-              className="text-zinc-500 hover:text-zinc-800 transition-colors"
+              className={classNames({
+                "text-zinc-900": link.href === currentPath,
+                "text-zinc-500": link.href !== currentPath,
+                "hover:text-zinc-800 transition-colors": true,
+              })}
             >
               {link.label}
             </Link>
---
+++ app/stories/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NewStoryPage = () => {
+  return <div>NewStoryPage</div>;
+};
+
+export default NewStoryPage;
