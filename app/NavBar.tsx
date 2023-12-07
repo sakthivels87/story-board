8e7a786b98de9219e1d4a4169c9495dbec992896
Sakthivel <sakthi.samuthiram@gmail.com>
2023-12-07 14:41:27 +0530

    Handling form submission changes.

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -12,11 +12,10 @@ const NavBar = () => {
     },
     {
       label: "Stories",
-      href: "/stories",
+      href: "/stories/*",
     },
   ];
   const currentPath = usePathname();
-  console.log(currentPath, "----current path");
   return (
     <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
       <Link href="/">
