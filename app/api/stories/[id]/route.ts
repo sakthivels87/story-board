b6eefe81585f0e768e608ddf94f4ea4d9af420bb
Sakthivel <sakthi.samuthiram@gmail.com>
2023-12-19 15:34:15 +0530

    assign the issue to users

--- app/api/stories/[id]/route.ts
+++ app/api/stories/[id]/route.ts
@@ -8,24 +8,27 @@ export async function PATCH(
   request: NextRequest,
   { params }: { params: { id: string } }
 ) {
-  // const session = await getServerSession(authOptions);
-  // if (!session) return NextResponse.json({}, { status: 401 });
+  //const session = await getServerSession(authOptions);
+  //if (!session) return NextResponse.json({}, { status: 401 });
   const body = await request.json();
   const valiation = pathStorySchema.safeParse(body);
   if (!valiation.success)
     return NextResponse.json(valiation.error.format(), { status: 400 });
 
-  const story = await prisma?.story.findUnique({
-    where: { id: parseInt(params.id) },
-  });
-  if (!story) return NextResponse.json("Not found", { status: 404 });
   const { title, description, assignedToUserId } = body;
+
   if (assignedToUserId) {
     const user = await prisma.user.findUnique({
       where: { id: assignedToUserId },
     });
-    if (!user) return NextResponse.json("User not found..", { status: 400 });
+    if (!user)
+      return NextResponse.json({ error: "Invalid user." }, { status: 400 });
   }
+  const story = await prisma?.story.findUnique({
+    where: { id: parseInt(params.id) },
+  });
+  if (!story) return NextResponse.json({ error: "Not found" }, { status: 404 });
+
   const updateStory = await prisma?.story.update({
     where: { id: story.id },
     data: {
