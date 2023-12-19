119526a2398b2d64b77bd11ec13a55380ba14561
Sakthivel <sakthi.samuthiram@gmail.com>
2023-12-19 20:02:36 +0530

    Adding new filtering component based on status of stories

---
+++ app/stories/list/StoryAction.tsx
@@ -0,0 +1,18 @@
+"use client";
+import { Button, Flex } from "@radix-ui/themes";
+import Link from "next/link";
+import React from "react";
+import StoryStatusFilter from "./StoryStatusFilter";
+
+const StoryAction = () => {
+  return (
+    <Flex justify="between" mt="5">
+      <StoryStatusFilter />
+      <Button>
+        <Link href="/stories/new">New Story</Link>
+      </Button>
+    </Flex>
+  );
+};
+
+export default StoryAction;
--- app/stories/list/loading.tsx
+++ app/stories/list/loading.tsx
@@ -1,41 +1,40 @@
-import prisma from "@/prisma/client";
 import { Box, Table } from "@radix-ui/themes";
-import StoryStatusBadge from "../components/StoryStatusBadge";
+import React from "react";
 import StoryAction from "./StoryAction";
-import Link from "./_components/Link";
+import Skeleton from "../../components/Skeleton";
 
-const NewStoryPage = async () => {
-  const stories = await prisma.story.findMany();
+const LoadingStoryPage = () => {
+  const stories = [1, 2, 3, 4, 5];
   return (
     <Box className="max-w-5xl">
-      <StoryAction />
-      <Table.Root className="mt-5" variant="surface">
+      <StoryAction></StoryAction>
+      <Skeleton />
+      <Table.Root className="mt-5">
         <Table.Header>
           <Table.Row>
-            <Table.ColumnHeaderCell>Story</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className="hidden md:table-cell">
-              Status
+            <Table.ColumnHeaderCell>
+              <Skeleton height="2rem" />
             </Table.ColumnHeaderCell>
             <Table.ColumnHeaderCell className="hidden md:table-cell">
-              Created At
+              <Skeleton height="2rem" />
+            </Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className="hidden md:table-cell">
+              <Skeleton height="2rem" />
             </Table.ColumnHeaderCell>
           </Table.Row>
         </Table.Header>
         <Table.Body>
           {stories.map((story) => {
             return (
-              <Table.Row key={story.id}>
+              <Table.Row key={story}>
                 <Table.RowHeaderCell>
-                  <Link href={`/stories/${story.id}`}>{story.title}</Link>
-                  <div className="block md:hidden">
-                    <StoryStatusBadge status={story.status} />
-                  </div>
+                  <Skeleton height="2rem" />
                 </Table.RowHeaderCell>
                 <Table.Cell className="hidden md:table-cell">
-                  <StoryStatusBadge status={story.status} />
+                  <Skeleton height="2rem" />
                 </Table.Cell>
                 <Table.Cell className="hidden md:table-cell">
-                  {story.description}
+                  <Skeleton height="2rem" />
                 </Table.Cell>
               </Table.Row>
             );
@@ -45,5 +44,5 @@ const NewStoryPage = async () => {
     </Box>
   );
 };
-export const dynamic = "force-dynamic";
-export default NewStoryPage;
+
+export default LoadingStoryPage;
