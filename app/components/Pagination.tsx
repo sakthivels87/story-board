3c435dcacda3bf460857999e6e25d122bc8aaa57
Sakthivel <sakthi.samuthiram@gmail.com>
2023-12-21 14:21:18 +0530

    Basic skeleton of pagination component is ready

---
+++ app/components/Pagination.tsx
@@ -0,0 +1,59 @@
+"use client";
+import { Button, Flex, Text } from "@radix-ui/themes";
+import { useRouter, useSearchParams } from "next/navigation";
+import React from "react";
+import {
+  MdChevronLeft,
+  MdChevronRight,
+  MdLastPage,
+  MdFirstPage,
+} from "react-icons/md";
+
+interface Props {
+  itemCount: number;
+  pageSize: number;
+  currentPage: number;
+}
+const Pagination = ({ itemCount, pageSize, currentPage }: Props) => {
+  const pageCount = Math.ceil(itemCount / pageSize);
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const changePage = (page: number) => {
+    const params = new URLSearchParams(searchParams);
+    params.set("page", page.toString());
+    router.push("?" + params.toString());
+  };
+
+  if (pageCount <= 1) return null;
+
+  return (
+    <Flex justify="center" gap="2">
+      <Text>
+        Page {currentPage} of {pageCount}
+      </Text>
+      <Button variant="soft" color="gray" onClick={() => changePage(1)}>
+        <MdFirstPage />
+      </Button>
+      <Button
+        variant="soft"
+        color="gray"
+        onClick={() => changePage(currentPage - 1)}
+      >
+        <MdChevronLeft />
+      </Button>
+      <Button
+        variant="soft"
+        color="gray"
+        onClick={() => changePage(currentPage + 1)}
+      >
+        <MdChevronRight />
+      </Button>
+      <Button variant="soft" color="gray" onClick={() => changePage(pageCount)}>
+        <MdLastPage />
+      </Button>
+    </Flex>
+  );
+};
+
+export default Pagination;
